f1cdf008de8f547406fabd6ffad94fea473c51bb
MDOUZANT1612 <douzant2303@gmail.com>
2025-05-18 17:07:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur DMN Network</h1>
+            <p>Votre univers sportif et professionnel, façon Canal+ et Netflix.</p>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
